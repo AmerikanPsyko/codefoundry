bcc95c4005ad6b0ed5dca6945f638ce0750fb428
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-03-09 12:47:46 -0800

    Added js code
    - Added Pokemon array include 4 pokemons, with 3 different keys

--- career-foundry/2.1/js/script.js
+++ career-foundry/2.1/js/script.js
@@ -1,6 +1,8 @@
-//Browser alert - Hello world popup
-alert('Hello World');
+let pokemonList = [
+    {name: "Bulbasaur", type: "Grass", height: 7}, // Key 1: Name of Pokemon
+    {name: "Charizard", type: "Fire", height: 5}, // Key 2: Type of Pokemon
+    {name: "Pikachu", type: "Electric", height: 3}, // Key 3: Size of pokemon
+    {name: "Evee", type: "Dark", height: 4}
+];
 
-//Favorite food - Write to DOM 
-let favFood = 'Pizza';
-document.write(favFood);
+console.log(pokemonList);
