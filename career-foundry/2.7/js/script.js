6f6683c0219ee756121a1c8d3ea96b8310623849
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-03-24 00:57:35 -0700

    Update script.js
    double check code

--- career-foundry/2.7/js/script.js
+++ career-foundry/2.7/js/script.js
@@ -1,88 +1,60 @@
 // Begin IIFE 
-   //Pokemon Repository list of Pokemon
-let pokemonRepository = (function (){
+//Pokemon Repository list of Pokemon
+let pokemonRepository = (function () {
   let pokemonList = [
-    {name: "Bulbasaur", type: "Grass", height: 7},
-    {name: "Charizard", type: "Fire", height: 5},
-    {name: "Pikachu", type: "Electric", height: 3}, 
-    {name: "Evee", type: "Dark", height: 4},
+    { name: "Bulbasaur", type: "Grass", height: 7 },
+    { name: "Charizard", type: "Fire", height: 5 },
+    { name: "Pikachu", type: "Electric", height: 3 },
+    { name: "Evee", type: "Dark", height: 4 },
   ];
 
-    //Function that will return all pokemon, retrieved from: pokemonList
+
   function getAll(pokemon) {
+    console.log("get all function")
     return pokemonList;
   };
 
-    //Function that will add pokemon, and push to: pokemonList
+
   function add(pokemon) {
+    console.log("add pokemon")
     pokemonList.push(pokemon);
   };
-    //Return for add/getAll
+
+
+
+
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  function addListener(button, pokemon) {
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    })
+  }
+
+
   return {
     add: add,
     getAll: getAll
   };
-  
-  })();
 
+})();
 
-function showDetails(pokemon) {
-    console.log(pokemon.name);
-    }
-
-  // Add listener for button click and then passes the function to show details (Pokemon name)
-    function addListener (button, pokemon) {
-      button.addEventListener ('click', function () {
-        showDetails(pokemon);
-      })
-    }
-
-
-  // Function that adds list items: Button, InnerText, and gets query from DOM for .pokemon-list. Also appends pokemonList to button 
-    function addListItem(pokemon) {
-    
-      // Updated loop from "for" to "forEach"
-    pokemonRepository.getAll().forEach (function (pokemon) {
-
-    // Calls on selecting 'pokemon-list' from DOM
+function addListItem(pokemon) {
+  pokemonRepository.getAll().forEach(function (pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
-
-    // Create element 'li'
     let listItem = document.createElement('li');
-
-    // Creates element 'button
     let button = document.createElement('button');
-
-    // Append button to 'ul'
     pokemonList.appendChild(button);
-
-    // Adds inner text to button using pokemon.name to pull from Repository
     button.innerText = pokemon.name;
-
-    // Adds class ID 'button-list/ to button
     button.classList.add('button-list');
-
-    // Appends 'li' to 'ul' 
     pokemonList.appendChild(listItem);
-     
-    // Adds listener for button click 
     addListener(button, pokemon);
-
   });
-    
-  
 
 
-
-    
-
-   
-
-  
-
-    //Add "Dragonite" to the pokemonList, logged in console and printed to DOM
-  pokemonRepository.add({name: 'Dragonite'});
-  console.log(pokemonRepository.getAll());
 }
-
 addListItem();
+
