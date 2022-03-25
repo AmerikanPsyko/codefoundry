61d936d4d2d558eb36b883aa5cd82f2ea886b9ee
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-03-25 16:10:59 -0700

    ccleand up code
    code cleanup

--- career-foundry/2.7/js/script.js
+++ career-foundry/2.7/js/script.js
@@ -8,53 +8,43 @@ let pokemonRepository = (function () {
     { name: "Evee", type: "Dark", height: 4 },
   ];
 
-  //Function that will return all pokemon, retrieved from: pokemonList
   function getAll(pokemon) {
     return pokemonList;
   }
 
-  //Function that will add pokemon, and push to: pokemonList
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
-  
-  function addListener(button, pokemon) {
-    button.addEventListener("click", function () {
-      showDetails(pokemon);
-    });
-  }
 
-  function showDetails(pokemon) {
-    console.log(pokemon.name);
-  }
-
-  function addListItem(pokemon) {
-    pokemonRepository.getAll().forEach(function (pokemon) {
-      let pokemonList = document.querySelector(".pokemon-list");
-
-      let listItem = document.createElement("li");
-
-      let button = document.createElement("button");
-
-      pokemonList.appendChild(button);
-
-      button.innerText = pokemon.name;
-
-      button.classList.add("button-list");
-
-      pokemonList.appendChild(listItem);
-
-      addListener(button, pokemon);
-    });
-  }
-
-  //Return for add/getAll
   return {
     add: add,
     getAll: getAll,
   };
-
-
-  addListItem();
 })();
 
+// End IIFE
+
+function showDetails(pokemon) {
+  console.log(pokemon.name);
+}
+
+function addListener(button, pokemon) {
+  button.addEventListener("click", function () {
+    showDetails(pokemon);
+  });
+}
+
+function addListItem(pokemon) {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    pokemonList.appendChild(button);
+    button.innerText = pokemon.name;
+    button.classList.add("button-list");
+    pokemonList.appendChild(listItem);
+    addListener(button, pokemon);
+  });
+}
+
+addListItem();
