cd08ae6f5c82eb74df1afca8cadc2c85ef999ebf
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-03-26 13:15:31 -0700

    Update script.js
    updated JS for fetch

--- career-foundry/2.7/js/script.js
+++ career-foundry/2.7/js/script.js
@@ -1,12 +1,25 @@
 // Begin IIFE
 //Pokemon Repository list of Pokemon
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: "Bulbasaur", type: "Grass", height: 7 },
-    { name: "Charizard", type: "Fire", height: 5 },
-    { name: "Pikachu", type: "Electric", height: 3 },
-    { name: "Evee", type: "Dark", height: 4 },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  //Other API functions start
+  function loadList() {
+    return fetch(apiUrl).then(function (response){
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon)
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
 
   function getAll(pokemon) {
     return pokemonList;
@@ -16,23 +29,25 @@ let pokemonRepository = (function () {
     pokemonList.push(pokemon);
   }
 
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  function addListener(button, pokemon) {
+    button.addEventListener("click", function () {
+      showDetails(pokemon);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
+    addListener: addListener,
+    loadList: loadList,
   };
 })();
 
 // End IIFE
-
-function showDetails(pokemon) {
-  console.log(pokemon.name);
-}
-
-function addListener(button, pokemon) {
-  button.addEventListener("click", function () {
-    showDetails(pokemon);
-  });
-}
 
 function addListItem(pokemon) {
   pokemonRepository.getAll().forEach(function (pokemon) {
@@ -43,8 +58,14 @@ function addListItem(pokemon) {
     button.innerText = pokemon.name;
     button.classList.add("button-list");
     pokemonList.appendChild(listItem);
-    addListener(button, pokemon);
+    pokemonRepository.addListener(button, pokemon);
   });
 }
+
+  pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+      pokemonRepository.addListItem(pokemon);
+    });
+  });
 
 addListItem();
