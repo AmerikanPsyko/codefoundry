cc3c7676716b20ca8e0f9587ace3e58a3b88b5d2
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-04-14 13:55:32 -0700

    work in progress
    resume work in progress

---
+++ career-foundry/startbootstrap-the-big-picture-gh-pages/js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - The Big Picture v5.0.5 (https://startbootstrap.com/template/the-big-picture)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+function openResume () {
+    let buttonTrigger = document.querySelector('button');
+    buttonTrigger.addEventListener('click', openResume)   
+}
+
+let buttonTrigger = document.querySelector('button');
+buttonTrigger.addEventListener('click')
